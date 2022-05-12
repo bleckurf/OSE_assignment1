3c5e0addb30030f6c3c6392ff3b726873d4b82d2
fannybleckur <fanny.bleckur@outlook.com>
2022-05-12 10:20:13 +0200

    Added test_quadratures.py

---
+++ test_quadratures.py
@@ -0,0 +1,9 @@
+from OSE_assignment1.quadratures import midpoint, trapezoidal
+
+
+def test_midpoint():
+    assert midpoint(0, 1, lambda x: x**2) == 0.25
+
+
+def test_trapezoidal():
+    assert trapezoidal(0, 1, lambda x: x**2) == 0.5
